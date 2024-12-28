9ebf571c5dc4331919b390ca851cce0795b57f94
ZolderOnGitHub <guimoi04@gmail.com>
2024-12-29 00:32:21 +0100

    preparing for riverwm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@ set fish_greeting
 
 abbr -a update "sudo pacman -Syu"
 abbr -a cls "clear"
+abbr -a odin "~/programs/odin/odin"
 
 function fish_prompt
     echo (set_color blue)(prompt_pwd) (set_color red)'λ '
